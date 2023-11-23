02e028b542d8acc136d30dfc70947c13f4f7974b
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2023-11-23 19:22:16 +0100

    core react snippet

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-// Import Dependencies
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+function render(reactElement, containerDOMElement) {
+  // create a DOM element
+  const domElement = document.createElement(reactElement.type);
+  // update properties
+  domElement.innerText = reactElement.children;
+  // domElement.setAttribute('href', reactElement.props.href);
+  for (const key in reactElement.props) {
+    const value = reactElement.props[key];
+    domElement.setAttribute(key, value);
+  }
 
-// Create react element
-const element = React.createElement('p', { id: 'hello' }, 'Hello World today');
+  // put it in the container
+  containerDOMElement.appendChild(domElement);
+}
 
-// Render application
-const container = document.querySelector('#root');
-const root = createRoot(container);
-root.render(element);
+const reactElement = {
+  type: 'a',
+  props: {
+    id: 'another link',
+    'data-number': 20,
+    href: 'https//google.com',
+  },
+  children: 'Read more on Wikipedia',
+};
+
+const containerDOMElement = document.querySelector('#root');
+
+render(reactElement, containerDOMElement);
